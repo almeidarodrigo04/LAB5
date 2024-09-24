1d5d28c1c1838d782d0d9d00fdd1732cdc18fd96
Felipe Cerri <felipecamargocerri@gmail.com>
2024-09-24 17:40:43 -0300

    correcao

--- part4.vhdl
+++ part4.vhdl
@@ -14,7 +14,7 @@ end entity part4;
 
 architecture Behaviour of part4 is
     component cont is
-        generic(n:natural:=4; k:natural:=15);
+        generic(n: natural := 4; k: natural := 15);
         port (
             Clk: in STD_LOGIC;
             Enable: in STD_LOGIC;
@@ -23,199 +23,104 @@ architecture Behaviour of part4 is
             rollover: out STD_LOGIC
         );
     end component;
+
+    signal pt: natural := 1;
+    signal q_signal: STD_LOGIC_VECTOR(26 downto 0); -- Para armazenar o valor de 'Q' do componente cont
+    signal rollover_signal: STD_LOGIC;
+
 begin
 
+    -- Instância única do componente cont
+    cont_inst: cont 
+        generic map(
+            n => 27,
+            k => pt
+        )
+        port map(
+            Clk => clock,
+            Enable => enable,
+            reset => RESET,
+            Q => q_signal,
+            rollover => rollover_signal
+        );
+
+    -- Processo de controle
     process(letra)
-        variable tam: NATURAL:=0;
-        variable pt: Natural:=1;
-        variable vec: STD_LOGIC_VECTOR (3 downto 0);
+        variable vec: STD_LOGIC_VECTOR (3 downto 0); -- Agora 'vec' é uma variável
     begin
-        if(enable='1' and reset='0') then
+        if (enable = '1' and RESET = '0') then
             case letra is 
                 when "000" => 
-                    tam:=2;
-                    vec:="10XX";
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '0';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        )
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => led
-                        );
-                    end loop;
+                    vec := "10XX";  -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= rollover_signal; -- Usa o rollover da instância para controlar o led
+
                 when "001" =>
-                    tam:=4;
-                    vec:="0111";
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '0';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        )
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => not led
-                        );
-                    end loop;
+                    vec := "0111"; -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= not rollover_signal;
+
                 when "010" =>
-                    tam:=4;
-                    vec:="0101"; 
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '0';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        )
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => not led
-                        );
-                    end loop;
+                    vec := "0101";  -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= not rollover_signal;
+
                 when "011" =>
-                    tam:=3;
-                    vec:="011X";
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '0';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        )
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => not led
-                        );
-                    end loop;
+                    vec := "011X";  -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= not rollover_signal;
+
                 when "100" =>
-                    tam:=1;
-                    vec:="1XXX";
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '0';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        );
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => not led
-                        );
-                    end loop;
+                    vec := "1XXX";  -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= not rollover_signal;
+
                 when "101" =>
-                    tam:=4;
-                    vec:="1101";
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        );
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => not led
-                        );
-                    end loop;
+                    vec := "1101";  -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= not rollover_signal;
+
                 when "110" => 
-                    tam:=3;
-                    vec:="001X";
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '0';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        );
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => not led
-                        );
-                    end loop; 
+                    vec := "001X";  -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= not rollover_signal;
+
                 when "111" =>
-                    tam:=4;
-                    vec:="1111";
-                    for i in 0 to tam-1 loop
-                        if(vec(i)='1') then
-                            pt := 25000000;
-                        else
-                            pt := 75000000;     
-                        end if;
-                        led <= '0';       
-                        c: cont 
-                        generic map(
-                            n => 27,
-                            k => pt
-                        )
-                        port map(
-                            Clk => clock,
-                            enable => enable,
-                            reset => reset,
-                            q =>
-                            rollover => not led
-                        );
-                    end loop;
+                    vec := "1111";  -- Atribuindo à variável
+                    if(vec(0) = '1') then
+                        pt <= 25000000;
+                    else
+                        pt <= 75000000;     
+                    end if;
+                    led <= not rollover_signal;
             end case;
         end if;
     end process; 
