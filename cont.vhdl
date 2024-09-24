3bff957df1db21990101d70a80830e923d28d970
almeidarodrigo04 <rodrigosilvalmeida10@gmail.com>
2024-09-24 17:18:27 -0300

    oq eu fiz?

--- cont.vhdl
+++ cont.vhdl
@@ -7,28 +7,36 @@ entity cont is
     generic(n:natural:=4; k:natural:=15);
     port (
         Clk: in STD_LOGIC;
+        Enable: in STD_LOGIC;
         reset: in STD_LOGIC;
         Q: out STD_LOGIC_VECTOR(n-1 downto 0);
-        rollover: out STD_LOGIC 
+        rollover: out STD_LOGIC
     );
 end entity;
 
 architecture Behaviour of cont is
     signal output: STD_LOGIC_VECTOR (n-1 downto 0);
+    signal seg: NATURAL :=0;
 begin
     process(Clk, reset)
     begin
         if(reset='1') then
             output <= (others => '0');
-        elsif(rising_edge(Clk)) then
-            if(output=k-1) then
-                rollover <= '1';
-                output <= (others => '0');
+            seg <= 0;
+        elsif (rising_edge(Clk)) then
+            if (Enable='1' and rollover='0') then
+                seg <= seg+1;
+                if (seg = k) then
+                    output <= (others => '0');
+                    rollover <= '1';
+                else
+                    output <= output+1;
+                    rollover <= '0';
+                end if;
             else
-                rollover <= '0';
-                output <= output+1;
+                seg <= seg;
             end if;
         end if;
     end process;
     Q <= output;
-end architecture;
+end architecture Behaviour;
